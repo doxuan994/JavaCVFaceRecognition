4ca7433ff3fafbbfc8153f5fb04680cc1163b592
Xuan Do <doxuancntp@gmail.com>
2018-06-13 13:30:23 -0400

    Initial commit

---
+++ app/src/main/java/com/doxua/www/javacvfacerecognition/MainActivity.java
@@ -0,0 +1,110 @@
+package com.doxua.www.javacvfacerecognition;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacv.AndroidFrameConverter;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.OpenCVFrameConverter;
+
+import java.io.IOException;
+
+import static org.bytedeco.javacpp.opencv_imgproc.CV_BGR2GRAY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    private static final int PICK_IMAGE = 100;
+    private ImageView imageView;
+    private TextView textView;
+
+    private opencv_objdetect.CascadeClassifier faceDetector;
+    private int absoluteFaceSize = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the image view and text view.
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.faces_value);
+
+        Button pickImageButton = (Button) findViewById(R.id.btnGallery);
+        pickImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery =
+                new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            Uri imageUri = data.getData();
+
+            // Convert to Bitmap.
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // Display the image.
+            imageView.setImageBitmap(bitmap);
+            // Detect faces and display number of faces in the text view.
+            detectAndDisplay(bitmap, textView);
+        }
+    }
+
+    void detectAndDisplay(Bitmap image, TextView facesValue) {
+
+        AndroidFrameConverter converterToBitmap = new AndroidFrameConverter();
+        OpenCVFrameConverter.ToMat converterToMat = new OpenCVFrameConverter.ToMat();
+
+        // Convert to Bitmap.
+        Frame frame = converterToBitmap.convert(image);
+        // Convert to Mat.
+        opencv_core.Mat mat = converterToMat.convert(frame);
+
+        // Load the CascadeClassifier class to detect objects.
+        faceDetector = TrainHelper.loadClassifierCascade(MainActivity.this, R.raw.frontalface);
+
+        // Convert to Gray scale.
+        cvtColor(mat, mat, CV_BGR2GRAY);
+        // Vector of rectangles where each rectangle contains the detected object.
+        opencv_core.RectVector faces = new opencv_core.RectVector();
+
+        // Detect the face.
+        faceDetector.detectMultiScale(mat, faces, 1.25f, 3, 1,
+                new opencv_core.Size(absoluteFaceSize, absoluteFaceSize),
+                new opencv_core.Size(4 * absoluteFaceSize, 4 * absoluteFaceSize));
+
+
+        // Count number of faces and display in text view.
+        int numFaces = (int) faces.size();
+        facesValue.setText(Integer.toString(numFaces));
+
+    }
+}
